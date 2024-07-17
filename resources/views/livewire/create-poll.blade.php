9019884f6047ce7b9ca019f7e4bb2962abfd0387
Edzer Dionido <jeddionido26@gmail.com>
2024-07-17 13:23:15 +0800

    add errors flash message

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -1,10 +1,11 @@
 <div>
+    <h2 class="text-2xl text-slate-1000 font-bold mb-5">Create a poll</h2>
     <form wire:submit.prevent="createPoll">
         @error('title')
         <p class="text-red-500">{{$message}}</p>
         @enderror
         <label>Poll Title</label>
-        <input type="text" wire:model.live="title" name="title" class="@error("title") border-red-500 @enderror"/>
+        <input type="text" wire:model.blur="title" name="title" class="@error("title") border-red-500 @enderror"/>
         Current title: {{$title}}
 
         <div class="mb-4 mt-4">
@@ -18,12 +19,12 @@
             <div class="mb-4">
                 <label> Option {{$k + 1 }} </label>
                 <div class="flex gap-2">
-                    <input type="text" wire:model="options.{{$k}}" class="@error("options.{$k}") border-red-500 @enderror"/>
+                    <input type="text" wire:model.blur="options.{{$k}}" class="@error("options.{$k}") border-red-500 @enderror"/>
                     <button class="btn" wire:click.prevent="removeOptions({{$k}})">Remove</button>
                 </div>
                 @error("options.{$k}")
-                <p>{{$message}}</p>
-            @enderror
+                <p class="text-red-500">{{$message}}</p>
+                @enderror
             </div>
             @endforeach
         </div>
